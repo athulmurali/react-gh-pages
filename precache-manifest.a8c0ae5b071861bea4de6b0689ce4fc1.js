3031d55114995bfa0946367cec7c576da8b8c051
Athul Muralidharan <athulmurali@gmail.com>
2019-01-19 01:03:19 -0500

    Updates

---
+++ precache-manifest.a8c0ae5b071861bea4de6b0689ce4fc1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce57df7328c45026792e",
+    "url": "/static/css/main.304d69e2.chunk.css"
+  },
+  {
+    "revision": "ce57df7328c45026792e",
+    "url": "/static/js/main.ce57df73.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7f9d93edf36cfa8749a83b36ca9d372b",
+    "url": "/index.html"
+  }
+];
